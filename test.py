347a15d0ad7f5091d826fd8fc500deeae5915400
franklinathk <frank.lin@int-tech.com.hk>
2024-06-27 07:30:30 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+result = 128 * 10**6 // iii36  # Calculate the result of the expression and use integer division
+binary_result = bin(result)  # Convert the result to binary
+print(result)
+print(binary_result)
